923ee653f7672f4045398f06f60be45087e59d26
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-24 17:43:09 -0500

    employee

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 -- delete table or drop
 -- this will crop/delete the tables everytime schema.sql is run
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
 
 
 
@@ -18,12 +20,11 @@ CREATE TABLE role (
     department_id INT
 );
 
-
--- -- add new table to buisness organizer database
--- CREATE TABLE employee (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     first_name VARCHAR(30),
---     lat_name VARCHAR(30),
---     role_id INT,
---     manager_id INT
--- );
+-- add new table to buisness organizer database
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    lat_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -20,3 +20,17 @@ VALUES
 ('Chief Financial Officer', 400000, 6),
 ('Controller', 100000, 6),
 ('Accountant', 73000, 6);
+
+INSERT INTO employee (first_name, lat_name, role_id, manager_id)
+VALUES
+('Arthur', 'Vandelay', 6, NULL),
+('Rusty', 'Shackleford', 3, 3),
+('Pepe', 'Sylvia', 1, 1),
+('Regina', 'Phalange', 10, NULL),
+('Jamie', 'Fraiser', 2, 2),
+('Acheron', 'Riverton', 8, NULL),
+('Kore', 'Springger', 7, NULL),
+('Dyonysis', 'Win Lee', 5, 5),
+('Zoe', 'Alexandria', 11, NULL),
+('Luffy', 'Monkley', 4, NULL),
+('Nami', 'Cathleen', 9, NULL);
